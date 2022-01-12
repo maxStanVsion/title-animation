d494e1e8af9cb0168339d4051abf8ec82dc6dbb6
maxStanVsion <96051328+maxStanVsion@users.noreply.github.com>
2022-01-12 13:52:39 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,11 @@ const heroTitle = document.querySelector(".hero__title");
 
 function splitWord(container, text) {
   const splitText = text.split(" ").map((word, i) => {
-    return `<span class="o-hidden hero__word-container"><span class="hero__word">${
-      !i ? "" : "&nbsp"
-    }${word}</span></span>`;
+    return `<span class="o-hidden hero__word-container"><span class="hero__word">${word}&nbsp;</span></span>`;
   });
 
-  container.innerHTML = splitText.join("");
-
+  container.innerHTML = splitText.join(" ");
+  console.log(container);
   // Added offset class in order ot check if words are wrapped
   let x = 0;
 
@@ -20,20 +18,34 @@ function splitWord(container, text) {
 
 splitWord(heroTitle, heroTitle.textContent);
 
-const heroWords = [...document.querySelectorAll('.hero__word')];
+/* -------Get New line ------------ */ 
+// const heroWords = [...document.querySelectorAll('.hero__word')];
+// const offsetTopHeroWords = heroWords.map((el) => el.className.split(' ')[1]);
+// const removeDuplicates = (arr) => arr.filter((item, i) => arr.indexOf(item) === i);
+// const y = removeDuplicates(offsetTopHeroWords);
 
-const offsetTopHeroWords = heroWords.map((el) => el.className.split(' ')[1]);
-const removeDuplicates = (arr) => arr.filter((item, i) => arr.indexOf(item) === i);
+// y.forEach((el,i)=> {
+  //   gsap.to(`.${el}`, {
+    //     delay: 0.3,
+    //     y: 0,
+    //     ease: Power4.easeOut,
+    //     duration: 0.5,
+    //     stagger: 0.1,
+    //   });
+    // })
+/* -------Get New line ------------ */ 
 
-const y = removeDuplicates(offsetTopHeroWords);
-
-y.forEach((el,i)=> {
-  gsap.to(`.${el}`, {
-    delay: 0.3,
-    y: 0,
-    ease: Power4.easeOut,
-    duration: 0.5,
-    stagger: 0.1,
-  });
-})
-
+gsap.to('.hero__word', {
+  delay: 0.3,
+  y: 0,
+  ease: Power4.easeOut,
+  duration: 0.5,
+  stagger: 0.03,
+});
+gsap.to('.hero__word', {
+  delay: 0.3,
+  opacity: 1,
+  ease: Power4.easeOut,
+  duration: 5,
+  stagger: 0.03,
+});
